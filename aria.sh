c5b9b076e83dbcc9494398facdbd290c38f79b73
Appucodex <80910976+Appucodex@users.noreply.github.com>
2021-04-27 19:10:01 +0530

    Update aria.sh

--- aria.sh
+++ aria.sh
@@ -1,7 +1,7 @@
 export MAX_DOWNLOAD_SPEED=0
-export MAX_CONCURRENT_DOWNLOADS=8
+export MAX_CONCURRENT_DOWNLOADS=25
 aria2c --enable-rpc --rpc-listen-all=false --rpc-listen-port 6800 \
   --max-connection-per-server=10 --rpc-max-request-size=1024M \
   --seed-time=0.01 --min-split-size=10M --follow-torrent=mem --split=10 \
-   --daemon=true --allow-overwrite=true --max-overall-download-limit=$MAX_DOWNLOAD_SPEED --bt-stop-timeout=600\
+   --daemon=true --allow-overwrite=true --max-overall-download-limit=$MAX_DOWNLOAD_SPEED --bt-stop-timeout=2700\
    --max-overall-upload-limit=1K --max-concurrent-downloads=$MAX_CONCURRENT_DOWNLOADS
